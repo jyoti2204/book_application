a68ab85056a40520888732f1f010e5d9676b6d59
jyoti0428 <81856876+jyoti0428@users.noreply.github.com>
2021-07-02 18:26:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/ripplr/bookstore_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ripplr.bookstore_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/book_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BookStoreApp extends StatefulWidget {
+  @override
+  _BookStoreAppState createState() => _BookStoreAppState();
+}
+
+class _BookStoreAppState extends State<BookStoreApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BookNavigationPage extends StatefulWidget {
+  @override
+  _BookNavigationPageState createState() => _BookNavigationPageState();
+}
+
+class _BookNavigationPageState extends State<BookNavigationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
